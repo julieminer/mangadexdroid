0ddb4cde927d124c595423ae52b56db6ce564a27
Julie Miner <julie.miner9@gmail.com>
2025-05-29 16:42:26 -0700

    Add read column to ReadQueueEntity

--- lib-database/src/main/java/com/melonhead/lib_database/read_queue/ReadQueueEntity.kt
+++ lib-database/src/main/java/com/melonhead/lib_database/read_queue/ReadQueueEntity.kt
@@ -6,6 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = ReadQueueDao.TABLE_NAME)
 data class ReadQueueEntity(
-    @PrimaryKey(autoGenerate = false) val id: String,
+    @PrimaryKey(autoGenerate = false) val chapterId: String,
+    @ColumnInfo(name = "read") val read: Boolean,
     @ColumnInfo(name = "retry_count") val retryCount: Int,
 )
