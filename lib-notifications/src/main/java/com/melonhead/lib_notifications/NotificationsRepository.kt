36c537ca6ee82e792458a4c6b56f8fd961cfb2de
Julie Miner <julie.miner9@gmail.com>
2025-06-04 14:55:30 -0700

    Fix for crash caused by logged out notification during app launch

--- lib-notifications/src/main/java/com/melonhead/lib_notifications/NotificationsRepository.kt
+++ lib-notifications/src/main/java/com/melonhead/lib_notifications/NotificationsRepository.kt
@@ -5,6 +5,7 @@ import androidx.core.app.NotificationManagerCompat
 import com.melonhead.lib_app_context.AppContext
 import com.melonhead.lib_app_data.AppData
 import com.melonhead.lib_app_events.AppEventsRepository
+import com.melonhead.lib_app_events.events.AppLifecycleEvent
 import com.melonhead.lib_app_events.events.AuthenticationEvent
 import com.melonhead.lib_app_events.events.UserEvent
 import com.melonhead.lib_database.chapter.ChapterDao
@@ -35,6 +36,8 @@ internal class NotificationsRepositoryImpl(
     private val authFailedNotificationChannel: AuthFailedNotificationChannel,
     private val newChapterNotificationChannel: NewChapterNotificationChannel,
 ): NotificationsRepository {
+    private var hasLaunched = false
+
     init {
         externalScope.launch {
             // refresh manga on login
@@ -43,7 +46,12 @@ internal class NotificationsRepositoryImpl(
                 appEventsRepository.events.collectLatest { event ->
                     launch {
                         when (event) {
+                            is AppLifecycleEvent.AppForegrounded -> {
+                                hasLaunched = true
+                            }
+
                             AuthenticationEvent.LoggedOut -> {
+                                if (!hasLaunched) return@launch
                                 if (appContext.isInForeground) return@launch
                                 val notificationManager = NotificationManagerCompat.from(context)
                                 if (!notificationManager.areNotificationsEnabled()) return@launch
