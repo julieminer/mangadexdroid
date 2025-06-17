f696c2327509ff1e1b504812b3b6eaa3858dac36
Julie Miner <julie.miner9@gmail.com>
2025-06-17 16:09:51 -0700

    Add comments for which settings to add

--- feature-settings/src/main/java/com/melonhead/feature_settings/SettingsActivity.kt
+++ feature-settings/src/main/java/com/melonhead/feature_settings/SettingsActivity.kt
@@ -5,9 +5,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.melonhead.feature_settings.viewmodels.SettingsViewModel
+import com.melonhead.lib_core.scenes.CloseBanner
 import com.melonhead.lib_core.theme.MangadexFollowerTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -19,7 +23,7 @@ internal class SettingsActivity : ComponentActivity() {
 
         setContent {
             MangadexFollowerTheme {
-
+                SettingsScreen(onTappedClose = { finish() })
             }
         }
 
@@ -27,9 +31,39 @@ internal class SettingsActivity : ComponentActivity() {
 
     companion object {
         internal fun newIntent(context: Context): Intent {
-            val intent = Intent(context, SettingsActivity::class.java)
-            return intent
+            return Intent(context, SettingsActivity::class.java)
         }
+    }
+}
+
+@Composable
+private fun SettingsScreen(
+    onTappedClose: () -> Unit = {},
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        CloseBanner(
+            title = "Settings",
+            hasDescription = false,
+            onDoneTapped = onTappedClose,
+            onSummaryTapped = { }
+        )
+
+        // manga list settings
+            // chapter sorting?
+            // showReadChapterCount
+
+        // render settings
+            // open in browser or webview
+            // native render
+                // use data saver
+                // chapterTapAreaSize
+
+        // automark as complete
+        // automark as reading
+        // notifications
+        // background updates
+
+        // logout
     }
 }
 
@@ -37,6 +71,6 @@ internal class SettingsActivity : ComponentActivity() {
 @Composable
 private fun ScreenPreview() {
     MangadexFollowerTheme {
-
+        SettingsScreen()
     }
 }
