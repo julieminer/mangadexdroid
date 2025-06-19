40a038bdf8c55c995626b8a114e917a6d09be9ef
Julie Miner <julie.miner9@gmail.com>
2025-06-19 14:39:51 -0700

    Wire up settings UI

--- feature-settings/src/main/java/com/melonhead/feature_settings/viewmodels/SettingsViewModel.kt
+++ feature-settings/src/main/java/com/melonhead/feature_settings/viewmodels/SettingsViewModel.kt
@@ -1,8 +1,17 @@
 package com.melonhead.feature_settings.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.melonhead.lib_app_data.AppData
+import com.melonhead.lib_app_events.AppEventsRepository
+import com.melonhead.lib_app_events.events.AuthenticationEvent
+import com.melonhead.lib_app_events.events.UserEvent
 
 internal class SettingsViewModel(
+    internal val appData: AppData,
+    private val appEventsRepository: AppEventsRepository,
 ): ViewModel() {
 
+    fun logout() {
+        appEventsRepository.postEvent(UserEvent.LogOut)
+    }
 }
