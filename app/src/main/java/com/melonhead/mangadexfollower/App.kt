cc726e6bf750f675f09802036e4a1ccc27e1a248
Julie Miner <julie.miner9@gmail.com>
2025-06-23 17:20:43 -0700

    Switch to repeating OneTimeTasks instead of periodic tasks

--- app/src/main/java/com/melonhead/mangadexfollower/App.kt
+++ app/src/main/java/com/melonhead/mangadexfollower/App.kt
@@ -4,14 +4,10 @@ import android.app.Application
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ProcessLifecycleOwner
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
 import com.melonhead.lib_app_context.AppContext
 import com.melonhead.lib_app_events.AppEventsRepository
 import com.melonhead.lib_app_events.events.AppLifecycleEvent
 import com.melonhead.lib_app_events.events.AuthenticationEvent
-import com.melonhead.lib_logging.Clog
 import com.melonhead.lib_networking.extensions.error401Callback
 import com.melonhead.mangadexfollower.di.AppModule
 import com.melonhead.mangadexfollower.work_manager.RefreshWorker
@@ -19,8 +15,6 @@ import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
-import kotlin.time.Duration.Companion.minutes
-import kotlin.time.toJavaDuration
 
 class App: Application() {
     private val appEventsRepository: AppEventsRepository by inject()
@@ -53,12 +47,7 @@ class App: Application() {
                 super.onStart(owner)
                 appContext.isInForeground = true
                 appEventsRepository.postEvent(AppLifecycleEvent.AppForegrounded)
-                Clog.i("Creating background task")
-                val refreshWorkRequest = PeriodicWorkRequestBuilder<RefreshWorker>(
-                    repeatInterval = 30.minutes.toJavaDuration(),
-                    flexTimeInterval = 15.minutes.toJavaDuration()
-                ).build()
-                WorkManager.getInstance(this@App).enqueueUniquePeriodicWork("refresh-task", ExistingPeriodicWorkPolicy.UPDATE, refreshWorkRequest)
+                RefreshWorker.scheduleDebugTask(this@App)
             }
 
             override fun onStop(owner: LifecycleOwner) {
--- app/src/main/java/com/melonhead/mangadexfollower/work_manager/RefreshWorker.kt
+++ app/src/main/java/com/melonhead/mangadexfollower/work_manager/RefreshWorker.kt
@@ -1,7 +1,13 @@
 package com.melonhead.mangadexfollower.work_manager
 
 import android.content.Context
+import androidx.work.Constraints
 import androidx.work.CoroutineWorker
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import com.melonhead.lib_app_events.AppEventsRepository
 import com.melonhead.lib_app_events.events.UserEvent
@@ -10,15 +16,47 @@ import kotlinx.coroutines.future.await
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.TimeUnit
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.toJavaDuration
 
-class RefreshWorker(appContext: Context, workerParams: WorkerParameters): CoroutineWorker(appContext, workerParams), KoinComponent {
+class RefreshWorker(
+    val appContext: Context,
+    workerParams: WorkerParameters
+): CoroutineWorker(appContext, workerParams), KoinComponent {
     private val appEventsRepository: AppEventsRepository by inject()
 
     override suspend fun doWork(): Result {
         val refreshCompletion = CompletableFuture<Unit>()
-        Clog.i("Refresh: refreshworker")
-        appEventsRepository.postEvent(UserEvent.RefreshManga(refreshCompletion))
+        Clog.i("Refresh: refreshworker start")
+        appEventsRepository.postEvent(UserEvent.RefreshManga(refreshCompletion, skipNetworkCheck = true))
+        Clog.i("Refresh: refreshworker waiting")
         refreshCompletion.await()
+        Clog.i("Refresh: refreshworker complete")
+
+        scheduleDebugTask(appContext)
+
         return Result.success()
+    }
+
+    companion object {
+        fun scheduleDebugTask(context: Context) {
+            Clog.i("Refresh: Scheduling debug task")
+            val debugWorkRequest = OneTimeWorkRequestBuilder<RefreshWorker>()
+                .setInitialDelay(15, TimeUnit.MINUTES)
+                .setConstraints(Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build())
+                .build()
+            WorkManager.getInstance(context).enqueue(debugWorkRequest)
+        }
+
+        fun scheduleRepeatingTask(context: Context) {
+            Clog.i("Refresh: Scheduling repeating task")
+            val refreshWorkRequest = PeriodicWorkRequestBuilder<RefreshWorker>(
+                repeatInterval = 30.minutes.toJavaDuration(),
+                flexTimeInterval = 15.minutes.toJavaDuration()
+            ).build()
+
+            WorkManager.getInstance(context).enqueueUniquePeriodicWork("refresh-task", ExistingPeriodicWorkPolicy.UPDATE, refreshWorkRequest)
+        }
     }
 }
