43be31924c8e96fd1d947bac47399afc64b58afc
Julie Miner <julie.miner9@gmail.com>
2025-07-29 10:16:54 -0700

    Fix status bar colour

--- lib-core/src/main/java/com/melonhead/lib_core/theme/Theme.kt
+++ lib-core/src/main/java/com/melonhead/lib_core/theme/Theme.kt
@@ -58,7 +58,7 @@ fun MangadexFollowerTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = if (useLightStatusColour) !darkTheme else darkTheme // negate darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = if (useLightStatusColour) darkTheme else !darkTheme // negate darkTheme
         }
     }
 
